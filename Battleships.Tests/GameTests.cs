9001f5f2a6e2c0d22290af53d199d1f87b22e386
Marcin <25091223+emwiechnik@users.noreply.github.com>
2020-09-02 23:37:06 +0200

    implemented testing for consecutive squares across specified axes

--- Battleships.Tests/GameTests.cs
+++ Battleships.Tests/GameTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using System;
 using Xunit;
 
 namespace Battleships.Tests
@@ -42,6 +43,52 @@ namespace Battleships.Tests
       var expectedBattleshipLength = 5;
 
       battleship.Length.Should().Be(expectedBattleshipLength);
+      var areSquaresConsecutive = AreSquaresConsecutiveStraightAcrossAxis(battleship, Axis.X) || AreSquaresConsecutiveStraightAcrossAxis(battleship, Axis.Y);
+      areSquaresConsecutive.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(true, Axis.X, "A1", "A2", "A3")]
+    [InlineData(true, Axis.X, "A3", "A2", "A1")]
+    [InlineData(false, Axis.X, "A1", "A2", "A4")]
+    [InlineData(false, Axis.X, "A1", "B2", "A3")]
+    [InlineData(true, Axis.Y, "A1", "B1", "C1")]
+    [InlineData(true, Axis.Y, "C1", "B1", "A1")]
+    [InlineData(false, Axis.Y, "A1", "B1", "D1")]
+    [InlineData(false, Axis.Y, "A1", "B2", "C1")]
+    [InlineData(false, Axis.Y, "A1", "B2", "C3")]
+    [InlineData(false, Axis.Y, "C3", "B2", "A1")]
+    public void AreSquaresConsecutiveAcrossAxis_Should_Return_Correct_Result(bool expected, Axis axis, params string[] gridSquares)
+    {
+      // act
+      var actual = AreSquaresConsecutiveStraightAcrossAxis(gridSquares, axis);
+
+      // assert
+      actual.Should().Be(expected);
+    }
+
+    private bool AreSquaresConsecutiveStraightAcrossAxis(string[] gridSquares, Axis axis)
+    {
+      var axisValue = (int)axis;
+      var otherAxisValue = (int)(axis == Axis.X ? Axis.Y : Axis.X);
+
+      for (var i = 0; i < gridSquares.Length - 1; ++i)
+      {
+        var diffOnAxis = Math.Abs((int)gridSquares[i][axisValue] - (int)gridSquares[i + 1][axisValue]);
+        var diffOnOtherAxis = Math.Abs((int)gridSquares[i][otherAxisValue] - (int)gridSquares[i + 1][otherAxisValue]);
+        if (diffOnAxis > 1 || diffOnOtherAxis > 0)
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    public enum Axis
+    {
+      Y = 0,
+      X = 1
     }
   }
 }
