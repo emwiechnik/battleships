de48ecfc1634acbf70c207d18327a8da6c51ea65
Marcin <25091223+emwiechnik@users.noreply.github.com>
2020-09-03 17:23:08 +0200

    added the possibility to play in the Console app

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Battleships.Enums;
+using System;
 
 namespace Battleships
 {
@@ -7,6 +8,42 @@ namespace Battleships
     static void Main(string[] args)
     {
       Console.WriteLine("Battleships!");
+
+      var board = new Board();
+      var computer = new Computer(board);
+
+      computer.PlaceShips();
+
+      var finished = false;
+      do
+      {
+        Console.Write("\nEnter the coordinates to shoot: ");
+        var coordinates = Console.ReadLine();
+
+        var shotResult = computer.MarkAShot(coordinates);
+        
+        var message = GetMessageBasedOnResult(shotResult);
+
+        Console.WriteLine(message);
+
+        if (computer.AllShipsSunk())
+        {
+          Console.WriteLine("\nAll the computer's ships have sunk!");
+          finished = true;
+        }
+      } while (!finished);
+    }
+
+    private static string GetMessageBasedOnResult(ShotResult shotResult)
+    {
+      switch (shotResult)
+      {
+        case ShotResult.Hit: return "A ship was hit!";
+        case ShotResult.Miss: return "That was a miss";
+        case ShotResult.Sink: return "A ship has sunk!";
+      }
+
+      return "No idea about the result..";
     }
   }
 }
