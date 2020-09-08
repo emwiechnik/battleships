149547830e0cab8ec16be29bc89fff29c6a188e9
Marcin <25091223+emwiechnik@users.noreply.github.com>
2020-09-08 23:22:22 +0200

    got square implementation

--- Battleships/Square.cs
+++ Battleships/Square.cs
@@ -6,7 +6,12 @@
     public int Row { get; set; }
     public Square(string squareString)
     {
-      
+      if (!string.IsNullOrWhiteSpace(squareString))
+      {
+        var rowParsed = int.TryParse(squareString.Substring(1), out int row);
+        Column = rowParsed ? squareString[0] : '\0';
+        Row = rowParsed ? row : 0;
+      }
     }
   }
 }
